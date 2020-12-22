5ba7696764c7dab7d78441dcca073db4ec2d9a38
danaSror <danayevd@post.bgu.ac.il>
2020-12-22 13:30:52 +0200

    update client and server

--- main.py
+++ main.py
@@ -8,8 +8,4 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    client.printC()
-    server.printS()
-    
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+   
