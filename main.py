8f72612e68c231ec8e100470ff88e96b6160c97d
Dana Sror <62503893+danaSror@users.noreply.github.com>
2020-12-22 12:54:33 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
