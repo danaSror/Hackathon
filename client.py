e9fd522e6f6f1dc1d765ea96fe2d55b115a3b487
Dana Sror <62503893+danaSror@users.noreply.github.com>
2020-12-30 18:19:10 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,56 +1,74 @@
 # The client is a single-threaded app, which has three states
 import socket
+import struct
+import threading
+from pynput.keyboard import Listener
+# from curtsies import Input
 
 
-# client configuration #
-TEAM_NAME = '-!-!-Rotem-&-Dana-!-!-'
-HOST = '127.0.0.1'                      # The server's hostname or IP address
-SERVER_PORT = 13117                     # The port used by the server
-BROADCAST = "255.255.255.255"
-OFFER_TIMEOUT = 1
+class Client:
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM ,socket.IPPROsocket.IPPROTO_UDP) # UDP
-client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #  Enable broadcasting mode
-client.bind(("", PORT))
+    # client configuration #                     
+    SERVER_PORT = 13117                     # The port used by the server
+    client_connection_list = []
+    buffer = 4096 #   TODO change names of boris
 
-# leave this state when you get an offer message
-def wait_for_server_offer():
-    client_sock.settimeout(OFFER_TIMEOUT)
-    try:
+    def __init__(self,teamName):
+        self.TEAM_NAME = teamName + '\n'
+        # UDP
+        self.client_socket_udf = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # UDP
+        #self.client_socket_udf.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        self.client_socket_udf.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #  Enable broadcasting mode
+        self.client_socket_udf.bind(("", Client.SERVER_PORT))
+        # TCP
+        self.conn_tcp = None
+    
+    # leave this state when you get an offer message
+    def wait_for_server_offer(self):
+        print("Client started, listening for offer requests...")
         while True:
-            (message, server_address) = client_socket.recvfrom(2048)
-    except socket.timeout:
-        return
+            data, addr = self.client_socket_udf.recvfrom(Client.buffer)
+            cookie, msg_type, tcp_port_number = struct.unpack('IBH', data)
+            if cookie == 0xfeedbeef and msg_type == 0x2 and tcp_port_number > 0:
+               print(f"Received offer from {addr[0]}, attempting to connect...")
+               tcp_server_port = int(tcp_port_number)
+               server_ip = addr[0]
+               tcp_thread = threading.Thread(target=self.execute_tcp_connection, args=(server_ip, tcp_server_port, self.TEAM_NAME )) #,name='TCP client')
+               tcp_thread.start()
+             
+        
 
+    def execute_tcp_connection(self,host_ip, tcp_server_port, team_name):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+                self.conn_tcp = client_socket
+                Client.client_connection_list.append(client_socket)
+                print("1")
+                client_socket.connect((host_ip, tcp_server_port))
+                print("2")
+                #client_socket.sendall(team_name.encode('utf-8'))
+                client_socket.sendto(team_name.encode('utf-8'),(host_ip,tcp_server_port))
+                print("3")
+                while True:
+                    data_from_server = client_socket.recv(Client.buffer)
+                    print("4")
+                    print(data_from_server.decode('utf-8'))
+                    # self.game_mode()
+        except:
+            print("Server disconnected, listening for offer requests...")
+            Client.client_connection_list.clear()
+            self.wait_for_server_offer()
 
-# leave this state when you successfully connect using TCP
-def connect_to_server():
-    pass
+              
 
+    # collect characters from the keyboard and send them over TCP. collect data from the network and print it onscreen
+    # def game_mode(self):
+    #     listener = Listener(on_press=on_press)
+    #     listener.start()
 
-# collect characters from the keyboard and send them over TCP. collect data from the network and print it onscreen
-def game_mode():
-    # Note that in game mode the client responds to two events: 1. both keyboard presses 2. data coming in over TCP.
-    # After connecting, the client sends the predefined team name to the server, followed by a line break (‘\n’).
-    # After that, the client simply prints anything it gets from the server onscreen
-    # sends anything it gets from the keyboard to the server
-    pass
+        
 
 
 if __name__ == "__main__":
-    
-    #client_socket.connect((HOST, PORT))
-    var = struct.pack('cc', 'd'.encode('ascii'), 'a'.encode('ascii'))
-    client_socket.sendto(var, (HOST, PORT))
-
-
-
-
-#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#    s.connect((HOST, PORT))
-#    print("client connected to server")
-#    s.sendall(b'Hello, world')
-#    data = s.recv(1024)
-
-#print('Received', repr(data))
+    client = Client('-!-!-Rotem-&-Dana-!-!-')
+    client.wait_for_server_offer()
